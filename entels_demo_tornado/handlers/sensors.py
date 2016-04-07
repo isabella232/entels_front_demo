cdfe6c83e017b7f2404f04e7ea15c6c1f565411d
Ivan Kavaliou <jownkiv@gmail.com>
2016-04-07 07:48:06 +0300

    added time label for terminating of recursion

--- entels_demo_tornado/handlers/sensors.py
+++ entels_demo_tornado/handlers/sensors.py
@@ -1,6 +1,7 @@
 import threading
 from random import randint
 
+import time
 from tornado.websocket import WebSocketHandler
 
 
@@ -10,19 +11,22 @@ class SensorsHandler(WebSocketHandler):
 
     def open(self):
         self.guids = []
+        self.thread_label = None
         print("WebSocket opened")
 
     def on_message(self, message):
         parts = message.split(' ')
         if len(parts) == 2:
+            thread_label = time.time()
+            self.thread_label = thread_label
             if parts[0] == 'GET_NEW':
-                self.get_new(parts[1])
+                self.get_new(thread_label, parts[1])
             if parts[0] == 'GET_OBJ':
-                self.get_new(parts[1])
+                self.get_new(thread_label, parts[1])
 
-    def get_new(self, guids=None):
+    def get_new(self, thread_label, guids=None):
         if guids:
-            self.guids = self.guids + guids.split(',')
+            self.guids = guids.split(',')
 
         result = {
             'objs': []
@@ -36,7 +40,8 @@ class SensorsHandler(WebSocketHandler):
                 ]
             })
         self.write_message(result)
-        threading.Timer(5, self.get_new).start()
+        if thread_label == self.thread_label:
+            threading.Timer(5, self.get_new, [thread_label]).start()
 
     def on_close(self):
         print("WebSocket closed")
